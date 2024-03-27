fc87efa6f83dbfda1a56f4d20b2b8aba2f799413
unknown <nataliatucka01@gmail.com>
2024-03-27 09:48:42 +0100

    Design of administration page

--- licencjat-frontend/src/admin/Admin.js
+++ licencjat-frontend/src/admin/Admin.js
@@ -1,9 +1,55 @@
+import { useNavigate } from "react-router-dom";
+import "./Admin.css";
+
 export default function Admin() {
+  const navigation = useNavigate();
+
+  const handleRedirect = (url) => {
+    navigation(url);
+  };
+
   return (
     <div className="content">
-      <p>
-        <a href="/recipes-panel">Panel przepisów</a>
-      </p>
+      <div className="container">
+        <div className="row p-3">
+          <p className="fs-3">Panel administracyjny</p>
+          <div className="col-12 col-md-6 col-lg-4 p-3">
+            <div
+              onClick={() => handleRedirect("/recipes-panel")}
+              className="module-box p-4"
+            >
+              <div className="module-content justify-content-center text-center">
+                <img src="recipes.png" className="module-img" />
+                <a href="/recipes-panel" className="ms-2">
+                  Panel przepisów
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-12 col-md-6 col-lg-4 p-3">
+            <div onClick={() => handleRedirect("#")} className="module-box p-4">
+              <div className="module-content justify-content-center text-center">
+                <img src="report.png" className="module-img" />
+                <a href="#" className="ms-2">
+                  Panel zgłoszeń
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-12 col-md-6 col-lg-4 p-3">
+            <div onClick={() => handleRedirect("#")} className="module-box p-4">
+              <div className="module-content justify-content-center text-center">
+                <img src="administration.png" className="module-img" />
+                <a href="#" className="ms-2">
+                  Panel Zarządzania
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
