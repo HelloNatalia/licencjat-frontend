69cf364723c1c9334cff131334f11651754a790d
unknown <nataliatucka01@gmail.com>
2024-04-14 16:58:43 +0200

    Added category photos

--- licencjat-frontend/src/icons-declaration.js
+++ licencjat-frontend/src/icons-declaration.js
@@ -151,6 +151,37 @@ const organicMarker = L.icon({
   popupAnchor: [-0, -33], // point from which the popup should open relative to the iconAnchor
 });
 
+export function getCategoryPhotoName(category_id) {
+  switch (category_id) {
+    case "b50911cb-569d-489d-befa-eed78def1d8f":
+      return "pieczywo-cat.png";
+    case "bd45d7a1-5811-4b85-8d5a-77839faca224":
+      return "owoce-cat.png";
+    case "ccda8028-94c5-46b2-ae5f-f1c0e162c0b8":
+      return "mieso-cat.png";
+    case "d305da6e-e2ed-4a4a-b57f-739c8688592d":
+      return "ryby-cat.png";
+    case "4fcd1ea2-785c-4d17-abee-83d0304c8161":
+      return "nabial-cat.png";
+    case "e9701efc-fd60-4fb4-bde8-fca56286e83d":
+      return "tluszcz-cat.png";
+    case "17103275-544e-46c2-80e7-4be554a674b1":
+      return "cukier-cat.png";
+    case "fb751a95-5662-4e3e-9e75-982d1830fecf":
+      return "napoje-cat.png";
+    case "0e8a8a52-73df-4032-ad59-cdcd6a47da3b":
+      return "mrozone-cat.png";
+    case "614f1d72-b876-4e64-bb86-94c943150b2b":
+      return "przetwory-cat.png";
+    case "9e73cb18-9501-4272-bb54-83fe393228bd":
+      return "dietetyczne-cat.png";
+    case "2b8bab7e-a3ce-43be-8dc0-00bb32413d2d":
+      return "organiczne-cat.png";
+    default:
+      return "pieczywo-cat.png";
+  }
+}
+
 // const greenMarker = L.icon({
 //   iconUrl: "markers/green-marker.png",
 //   // shadowUrl: "leaf-shadow.png",
